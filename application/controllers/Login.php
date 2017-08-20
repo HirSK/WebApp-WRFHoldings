45924f8aeb4089693c187cba617b076739145ebd
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2017-08-20 19:42:28 +0530

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -36,10 +36,60 @@ class Login extends CI_Controller{
 		$userInfo['UserPassword'] = $userpass;
 
 		$res = $this->gen_model->userAuth($userInfo);
+        //print_r($res);
 
-		if($res=='TRURE'){
-			redirect('/welcome');
-		}else {
+        $currentUserId =  $res[0]->idUser;
+
+		// if($res=='TRURE'){
+		// 	redirect('/welcome');
+		// }
+        if($currentUserId){
+
+            $whereArray = array('User_idUser' => $currentUserId);
+            $columnArray = array('Employee_Outlet_idOutlet','Employee_idEmployee');
+
+            $data = $this->gen_model->getData($tablename='supervisor',$columns_arr = $columnArray,$where_arr = $whereArray);
+
+            //getting the loggers information for the session
+            $loggerEmpID = $data[0]->Employee_idEmployee;//receives the loggers employee id
+            $loggerOutlet = $data[0]->Employee_Outlet_idOutlet;//receives the loggers outlet
+
+            $whereArray2 = array('idEmployee' => $loggerEmpID);
+            $whereArray3 = array('idOutlet' => $loggerOutlet );
+
+
+            //receives the loggers information
+            $loggerEmployeeData = $this->gen_model->getData($tablename='employee',$columns_arr = array(),$where_arr = $whereArray2);
+            //receives the loggers outlet information
+            $loggerOutletData = $this->gen_model->getData($tablename='outlet',$columns_arr = array(),$where_arr = $whereArray3);
+
+            //print_r($loggerEmployeeData);
+            //print_r($loggerOutletData);
+            //redirect('/welcome'.$data);
+            
+            
+            //die();
+
+            //creating a session
+
+            $loggerName = $loggerEmployeeData[0]->EmployeeNameWithInitials;
+            $loggerEmail = $loggerEmployeeData[0]->EmployeeEmail;
+            $loggerOutletName = $loggerOutletData[0]->Outletname;
+            $loggerOutletLocation = $loggerOutletData[0]->OutletLocation;
+
+            $the_session = array("loggerID"=>$loggerEmpID,"loggerOutletID"=>$loggerOutlet,"loggerName" => $loggerName, "loggerEmail" => $loggerEmail,"loggerOutletName"=>$loggerOutletName,"loggerOutletLocation"=>$loggerOutletLocation);
+            $this -> session -> set_userdata($the_session);
+
+            //echo $this->session->userdata('loggerOutletLocation');
+            
+            //$this->session->set_userdata('userID' ,$loggerEmpID );
+            //echo "session user".$this->session->userdata('userID');
+
+            //die();
+
+            redirect('/welcome');
+        }
+        else {
             echo "
                 <div>
                 <center>
