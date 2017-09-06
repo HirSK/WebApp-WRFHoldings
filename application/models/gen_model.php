517f06d5e924383786351f075ec7cbf045d302fc
hima1993 <himarukrox@gmail.com>
2017-09-06 23:55:33 +0530

    Update gen_model.php

--- application/models/gen_model.php
+++ application/models/gen_model.php
@@ -119,4 +119,27 @@ class gen_model extends CI_Model{
          return $q->result();
             
         }
+	
+	public function updateData($tablename, $data_arr, $where_arr) {
+        try {
+
+
+
+
+            $this->db->update($tablename, $data_arr, $where_arr);
+            $report = array();
+            $report['error'] = $this->db->_error_number();
+            $report['message'] = $this->db->_error_message();
+            return $report;
+        } catch (Exception $err) {
+
+            return $err->getMessage();
+        }
+    }
+
+
+public function deleteData($tblName,$where){
+      return $this->db->delete($tblName,$where);
+}
+
 }
