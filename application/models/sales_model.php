4bf1ce53ccb368b3d5bd227b5bc2f0d0a5c27eb7
HirSK <hirukarunathilaka@gmail.com>
2017-12-04 09:59:11 +0530

    completed getInvoiceData and updatedInvoiceData functions

--- application/models/sales_model.php
+++ application/models/sales_model.php
@@ -18,4 +18,12 @@ class sales_model extends CI_Model{
 		$result = $query->result();
 		return $result;
 	}
+
+
+	function updateInvoiceData($id,$invoice_array){
+
+		$this->db->where('idInvoice', $id);
+        $ret = $this->db->update('invoice', $invoice_array);
+        return $ret;
+	}
 }
