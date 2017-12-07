1e6e728aabf8d2f6e8500d2d3a4129fa9b497201
HirSK <hirukarunathilaka@gmail.com>
2017-12-07 20:14:54 +0530

    added new js functions

--- application/views/Attendance_view.php
+++ application/views/Attendance_view.php
@@ -24,11 +24,8 @@
         <section>
         <div class="wizard">
             
-            <form role="form">
+           <!--  <form role="form"> -->
                 
-
-
-
                             <section class="content">
 
 
@@ -39,9 +36,9 @@
                                             <!-- text input -->
                                             <div class="row" style="padding-left: 12px;">
                                                 <div class="col-md-4">
-                                                    <label>Month</label>
+                                                    <label>Enter year and month</label>
 
-                                                    <input type="text" class="form-control" id="month" placeholder="Enter month name" onkeypress="Javascript: if (event.keyCode==13) getDetails();">
+                                                    <input type="text" class="form-control" id="month" placeholder="EX: 2017-12" onkeypress="Javascript: if (event.keyCode==13) getDetails();">
                                                     <br>
                                                 </div>
 
@@ -67,7 +64,7 @@
 
 
 
-                                   <form id="myform" method="post">
+                                    <!-- <form id="myform" method="post">
                                        
                                         
                                     <div class="col-md-11">
@@ -82,11 +79,10 @@
                                         <th>Employee Full Name</th>
                                         <th>Name with Initials</th>
                                         <th>NIC Number</th>
-                                        <th>Number of Leaves Days</th>
-                                        <th>Number of Attended Days</th>
+                                        <th>Number of Half Days</th>
+                                        <th>Number of full Days</th>
+                                        <th>Number of leaves</th>
                                         
-
-
 
                                     </tr>
 
@@ -95,17 +91,20 @@
                                              {  
                                                 ?><tr>
                                                 <td><input name="checkbox[]" class="case" type="checkbox" /></td>  
-                                                <td><?php echo $row->idEmployee;?></td>  
+                                                <td id="id"><?php echo $row->idEmployee;?></td>  
                                                 <td><?php echo $row->EmployeeFullName;?></td> 
                                                 <td><?php echo $row->EmployeeNameWithInitials;?></td>  
                                                 <td><?php echo $row->EmployeeNIC;?></td>
 
                                                 <td>
-                                                    <input type="text" class="form-control" name="">
+                                                    <input type="text" class="form-control" name="half" id="half" readonly="">
                                                 </td>
                                                 <td>
-                                                    <input type="text" class="form-control" name="">
-                                                </td> 
+                                                    <input type="text" class="form-control" name="full" id="full" readonly="">
+                                                </td>
+                                                 <td>
+                                                    <input type="text" class="form-control" name="leave" id="leave" readonly="">
+                                                </td>  
                                                 
                                                     
                                              <?php }  
@@ -115,9 +114,29 @@
                                            
                                         </table>
                                          </div>
-                                    </form>
-                                   
-                                  
+                                    </form>  -->
+                        <div class="col-sm-12">
+                            <h5>Preview:</h5>
+                            <div class="row">
+                                <div class="col-xs-12">
+                                    <div class="table-responsive">
+                                        <table class="table preview-table" id="list">
+                                            <thead>
+                                                <tr>
+                                                    <th>Emp:ID</th>
+                                                    <th>Full</th>
+                                                    <th>Half</th>
+                                                    <th>Leave</th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody></tbody> <!-- preview content goes here-->
+                                        </table>
+                                    </div>                            
+                                </div>
+                            </div>
+                           
+                        </div>
 
                             
                             </section>
@@ -132,7 +151,7 @@
                     
                     <div class="clearfix"></div>
                
-            </form>
+            <!-- </form> -->
         </div>
     </section>
 
@@ -241,17 +260,18 @@ jQuery('#master').on('click', function(e) {
         
             $.ajax({
                 type : "post",
-                url: "<?php echo base_url().'index.php/runPayrollController/getDays'?>",
+                url: "<?php echo base_url().'index.php/AttendanceController/getDays'?>",
                 cache: false,
                 data : {month :  $('#month').val()},
                 success : function(json){
                     var obj=jQuery.parseJSON(json);
+                    // alert(obj);
                     // var obj=json;
-                    alert(obj[0].workingDays);
+                    // alert(obj[0].workingDays);
 
                     if(obj){
-                        $('#working_days').val();
-                        $('#holidays').val();
+                        $('#working_days').val(obj[0].workingDays);
+                        $('#holidays').val(obj[0].holidays);
                                                
                     }else{
                         alert("something wrong");
@@ -260,6 +280,60 @@ jQuery('#master').on('click', function(e) {
 
                 },
             });
+            $.ajax({
+                type : "post",
+                url: "<?php echo base_url().'index.php/AttendanceController/getEmployeeAttendance'?>",
+                cache: false,
+                data : {month :  $('#month').val()},
+                success : function(json){
+                    var obj=jQuery.parseJSON(json);
+                    
+
+                          // alert(obj[1].full);
+                    // console.log(json);
+
+                    if(obj){
+                        // $('#working_days').val(obj[0].full);
+                        // $('#holidays').val(obj[0].half);
+                        // alert(obj[0].full);
+                         // alert(obj[0].half);
+
+                         // var workingDays = parseInt($('#working_days').val());
+
+                        for (var i=0;i<obj.length;i++) {
+                            var workingDays = parseInt($('#working_days').val());
+                            var rows = "";
+                            var id = parseInt(obj[i].Employee_idEmployee);
+                            var full = parseInt(obj[i].full);
+                             var half = parseInt(obj[i].half);
+                             var leave = workingDays-(full+half);
+
+                            rows += "<tr><td>" + id + "</td><td>" + full + "</td><td>" + half + "</td><td>" + leave + "</td></tr>";
+                            $(rows).appendTo("#list tbody");
+                            
+                        }
+
+
+                        // for (i=0; i < obj.length; i++) {
+                        //     $('#full').append("<td>"+obj[i].full+"</td>");
+                        //     $('#half').append("<td>"+obj[i].half+"</td>");
+                        // }
+
+                        // for (i=0; i < obj.length; i++) {
+                        //     $('#full').html(obj[i].full);
+                        //     $('#half').html(obj[i].half);
+                        // }
+                                               
+                    }else{
+                        alert("something wrong");
+                        // clearTextBoxes();
+                    }
+
+                },
+            });
+
+
+
         }else{
             alert("Please enter the month name and hit enter");
             // clearTextBoxes();
