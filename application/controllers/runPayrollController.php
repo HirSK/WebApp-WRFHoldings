4b00800c70a2d25964e264f92fc28d48e388ae96
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2017-12-09 10:21:37 +0530

    added getSalaryDetails method

--- application/controllers/runPayrollController.php
+++ application/controllers/runPayrollController.php
@@ -16,6 +16,29 @@ class RunPayrollController extends BaseController{
 
     }
 
+    public function getSalaryDetails(){
+
+        $empID = $this->input->post('empID');
+        $empName = $this->input->post('empName');
+        $full = (int)$this->input->post('full');
+        $half = (int)$this->input->post('half');
+        $working = (int)$this->input->post('working');
+
+        $emp_working = $full + ($half*0.5);
+
+        $query_res = $this->payroll_model->getBasicSalary($empID);
+        
+        $basic_salary=$query_res[0]->SalaryBasic;
+
+        $salary_per_day = floatval($basic_salary)/$working;
+        $emp_gross_salary = $salary_per_day*$emp_working;
+
+        $arr = array('id'=>$empID,'empName'=>$empName, 'basic_salary' => $basic_salary, 'salary_per_day' => $salary_per_day, 'emp_gross_salary' => $emp_gross_salary);
+
+        echo json_encode($arr);
+        
+    }
+
     
     
     
