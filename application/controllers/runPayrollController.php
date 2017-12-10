7fe432856b38f02d26955d2e920944c9eeb1ac46
Hirunika karunathilaka <hirukarunathilaka@gmail.com>
2017-12-10 21:05:34 +0530

    modified the setSalary function

--- application/controllers/runPayrollController.php
+++ application/controllers/runPayrollController.php
@@ -44,16 +44,37 @@ class RunPayrollController extends BaseController{
 
         $empID = $this->input->post('empID');
         $empName = $this->input->post('empName');
-        $gross = (int)$this->input->post('gross');
-        $food = (int)$this->input->post('food');
-        $holiday = (int)$this->input->post('holiday');
-        $deduct = (int)$this->input->post('deduct');
+        $gross = floatval($this->input->post('gross'));
+        $food = floatval($this->input->post('food'));
+        $holiday = floatval($this->input->post('holiday'));
+        $incentive = floatval($this->input->post('incentive'));
 
-        $total_allowance = $food+$holiday;
+        $total_allowance = $food+$holiday+$incentive;
 
         $query_res = $this->payroll_model->getBasicSalary($empID);
+
         $etf = $query_res[0]->Salary_ETF_Amount;
-        $total_deduct = $deduct+floatval($etf);
+        $basic_salary = $query_res[0]->SalaryBasic;
+        // $total_deduct = $deduct+floatval($etf);
+        $total_deduct = $etf;
+
+        $netSalary = ($gross + $total_allowance) - $total_deduct;
+
+
+       $salary = array(
+                       'Employee_idEmployee' => $empID,                       
+                        'Salary' =>$netSalary,
+                        'SalaryDeductedAmount' =>$total_deduct ,
+                        'SalaryAllowanceAmount' =>$total_allowance 
+                        );
+
+       $res = $this->payroll_model->SetSalary($salary);
+
+       $salary_id = $res;
+
+       $arr = array('id'=>$empID,'salary_id'=>$salary_id,'empName'=>$empName, 'basic_salary' => $basic_salary, 'total_deduct' =>$total_deduct, 'emp_gross_salary' =>$gross,'total_allowance'=>$total_allowance,'netSalary' => $netSalary);
+
+       echo json_encode($arr);
 
         
         // $this->payroll_model->addAllowance()
